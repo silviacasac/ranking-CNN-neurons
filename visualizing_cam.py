df3ca8537e475e8cbdaa76338adfb9916b5e9a33
silviacasac <43185453+silviacasac@users.noreply.github.com>
2021-06-19 20:32:33 +0200

    Create visualizing_cam.py

---
+++ visualizing_cam.py
@@ -0,0 +1,108 @@
+""" Producing CAM visualizations with ResNet-18 """
+# Following the code from https://github.com/zhoubolei/CAM
+# Uses PyTorch
+# Example for ResNet-18 and using ImageNet ULRs
+
+import io
+import requests
+from PIL import Image
+from torchvision import models, transforms
+from torch.autograd import Variable
+from torch.nn import functional as F
+import numpy as np
+import cv2
+import pdb
+
+# Input image
+LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
+IMG_URL = 'http://static.flickr.com/98/241057925_f4c5883aac.jpg'
+
+# Networks such as googlenet, resnet, densenet already use global average pooling at the end, so CAM could be used directly.
+model_id = 2
+if model_id == 1:
+    net = models.squeezenet1_1(pretrained=True)
+    finalconv_name = 'features' # this is the last conv layer of the network
+elif model_id == 2:
+    net = models.resnet18(pretrained=True)
+    finalconv_name = 'layer4'
+elif model_id == 3:
+    net = models.densenet161(pretrained=True)
+    finalconv_name = 'features'
+elif model_id==4:
+    net = models.vgg16(pretrained=True)
+    finalconv_name = 'features'
+elif model_id == 5:
+    net = models.densenet121(pretrained=True)
+    finalconv_name = 'features'
+
+# All available architectures are: https://pytorch.org/docs/stable/torchvision/models.html 
+
+net.eval()
+
+# Hook the feature extractor
+features_blobs = []
+def hook_feature(module, input, output):
+    features_blobs.append(output.data.cpu().numpy())
+
+net._modules.get(finalconv_name).register_forward_hook(hook_feature)
+
+# Get the softmax weight
+params = list(net.parameters())
+weight_softmax = np.squeeze(params[-2].data.numpy())
+
+def returnCAM(feature_conv, weight_softmax, class_idx):
+    # Generate the class activation maps upsample to 256x256
+    size_upsample = (256, 256)
+    bz, nc, h, w = feature_conv.shape
+    output_cam = []
+    for idx in class_idx:
+        cam = weight_softmax[idx].dot(feature_conv.reshape((nc, h*w)))
+        cam = cam.reshape(h, w)
+        cam = cam - np.min(cam)
+        cam_img = cam / np.max(cam)
+        cam_img = np.uint8(255 * cam_img)
+        output_cam.append(cv2.resize(cam_img, size_upsample))
+    return output_cam
+
+
+normalize = transforms.Normalize(
+   mean=[0.485, 0.456, 0.406],
+   std=[0.229, 0.224, 0.225]
+)
+preprocess = transforms.Compose([
+   transforms.Resize((224,224)),
+   transforms.ToTensor(),
+   normalize
+])
+
+response = requests.get(IMG_URL)
+img_pil = Image.open(io.BytesIO(response.content))
+img_pil.save('test.jpg')
+
+img_tensor = preprocess(img_pil)
+img_variable = Variable(img_tensor.unsqueeze(0))
+logit = net(img_variable)
+
+# Download the imagenet category list
+classes = {int(key):value for (key, value)
+          in requests.get(LABELS_URL).json().items()}
+
+h_x = F.softmax(logit, dim=1).data.squeeze()
+probs, idx = h_x.sort(0, True)
+probs = probs.numpy()
+idx = idx.numpy()
+
+# Output the prediction
+for i in range(0, 5):
+    print('{:.3f} -> {}'.format(probs[i], classes[idx[i]]))
+
+# Generate class activation mapping for the top1 prediction
+CAMs = returnCAM(features_blobs[0], weight_softmax, [idx[0]])
+
+# Render the CAM and output
+print('output CAM.jpg for the top1 prediction: %s'%classes[idx[0]])
+img = cv2.imread('test.jpg')
+height, width, _ = img.shape
+heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+result = heatmap * 0.3 + img * 0.5
+cv2.imwrite('CAM.jpg', result)
